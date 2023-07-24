76367e6e72e67ab4e993086c627b0ee0e7f60ca5
Mark Ruvald Pedersen <mped@demant.com>
2023-07-24 17:13:54 +0200

    Add check for artifact existance

--- src/artifact.py
+++ src/artifact.py
@@ -152,6 +152,9 @@ def parse_commit_msg(commit_msg):
 
 def create_artifact_commit(rbgit, artifact_name: str, binpath: str, ttl: str = "30 days") -> str:
     """ Create Artifact: A binary commit, with builtin traceability and expiry """
+    if not os.path.exists(binpath):
+        raise RuntimeError(f"Artifact '{binpath}' does not exist!")
+
     artifact_name_sane = sanitize_branch_name(artifact_name)
     if artifact_name != artifact_name_sane:
         print(f"Warning: Sanitized '{artifact_name}' to '{artifact_name_sane}'.", file=sys.stderr)
@@ -162,7 +165,6 @@ def create_artifact_commit(rbgit, artifact_name: str, binpath: str, ttl: str = "
     d['binpath'] = binpath
     d['ttl'] = ttl
 
-    # TODO: Test for binpath existence
     d['src_remote_name'] = "origin"    # TODO: Expose as argument
     d['src_sha']          = exec(["git", "rev-parse", "HEAD"])  # full sha
     d['src_sha_short']    = exec(["git", "rev-parse", "--short", "HEAD"])  # human readable
