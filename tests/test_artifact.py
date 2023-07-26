b35044d184f183590faf17b26052171e4ae1ee95
Mark Ruvald Pedersen <mped@demant.com>
2023-07-26 15:05:45 +0200

    pytest: Fix test case for gerrit ChangeID

--- tests/test_artifact.py
+++ tests/test_artifact.py
@@ -10,7 +10,7 @@ def test_prefix_lines():
 
 def test_extract_gerrit_change_id():
     commit_message = "Some changes\n\nChange-Id: I8473b920f2b0d34f2ef5ddbfdf3bea5db12c99d7"
-    assert artifact.extract_gerrit_change_id(commit_message, "Change Id: ") == "Change Id: I8473b920f2b0d34f2ef5ddbfdf3bea5db12c99d7"
+    assert artifact.extract_gerrit_change_id(commit_message) == "I8473b920f2b0d34f2ef5ddbfdf3bea5db12c99d7"
 
 def test_string_trunc_ellipsis():
     assert artifact.string_trunc_ellipsis(5, "hello world") == "he..."
