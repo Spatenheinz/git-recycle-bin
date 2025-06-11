d0950e5f5479443de3c4366daf10ab0de4bbb5a6
Mark Ruvald Pedersen <wabsie@gmail.com>
2025-06-11 23:02:04 +0200

    nix: pin nixpkgs in default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,7 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? (import (builtins.fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/tarball/24.05";
+    sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
+  }) {} ) }:
 
 pkgs.python311Packages.buildPythonApplication rec {
   pname = "git-recycle-bin";
